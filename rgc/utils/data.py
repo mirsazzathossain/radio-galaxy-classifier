19dbdada32defef0893a79be43408140dbe59438
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2024-09-14 13:37:26 +0000

    feat: implement catalog fetching from Vizier
    - Add `fetch_catalog` function to retrieve catalogs from Vizier service.
    - Implement `_UnsupportedServiceError` exception class for unsupported services.
    - Add tests for successful retrieval from Vizier and error handling for unsupported services.
    
    Closes #1

---
+++ rgc/utils/data.py
@@ -0,0 +1,42 @@
+"""
+A collection of utility functions for data manipulation.
+
+This module contains a collection of utility functions for astronomical data
+manipulation.
+"""
+
+__author__ = "Mir Sazzat Hossain"
+
+
+import pandas as pd
+from astroquery.vizier import Vizier
+
+
+class _UnsupportedServiceError(Exception):
+    """
+    An exception to be raised when an unsupported service is provided.
+    """
+
+    def __init__(self) -> None:
+        super().__init__("Unsupported service provided. Only 'Vizier' is supported.")
+
+
+def fetch_catalog(name: str, service: str = "Vizier") -> pd.DataFrame:
+    """
+    Fetch a catalog from a given astronomical service e.g. VizieR, Simbad.
+
+    :param name: The name of the catalog to be fetched.
+    :type name: str
+
+    :param service: The name of the astronomical service to be used.
+    :type service: str
+
+    :return: A pandas DataFrame containing the fetched catalog.
+    :rtype: pd.DataFrame
+    """
+    if service == "Vizier":
+        Vizier.ROW_LIMIT = -1
+        catalog = Vizier.get_catalogs(name)
+        return catalog[0].to_pandas()
+    else:
+        raise _UnsupportedServiceError()
